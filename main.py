407d07ce16552fdec07a1148ece88fd632433407
Mario Niemann <mario.niemann@t-online.de>
2018-11-22 21:56:54 +0100

    appinsights

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from flask_restful import Api
 from flask_jwt import JWT, jwt_required, JWTError
 from resources.feedback import Feedback
 import admin.startup
+from applicationinsights.flask.ext import AppInsights
 
 from resources.security import authenticate, indentity
 
 app = Flask(__name__)
 app.secret_key = "das ist mein eigener test"
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = '624b7480-cc70-449f-ac63-60410783b680'
+appinsights = AppInsights(app)
 
 
 api = Api(app)
